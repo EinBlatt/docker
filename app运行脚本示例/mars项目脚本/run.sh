79606f5efde6ccbaf8c81cda9c3462c3b3a2efff
einblatt <einblatt@163.com>
2020-04-01 10:03:19 +0800

    commit

--- app运行脚本示例/mars项目脚本/run.sh
+++ app运行脚本示例/mars项目脚本/run.sh
@@ -3,14 +3,14 @@
 
 echo "mars-service-redis-provider"
 docker-compose -f redis-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-service-redis-provider:v1.0.0 
+docker rmi ip:5000/mars-service-redis-provider:v1.0.0 
 docker-compose -f redis-docker-compose.yml up -d
 
 sleep 20
 
 echo "mars-service-gateway"
 docker-compose -f gw-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-service-gateway:v1.0.0   
+docker rmi ip:5000/mars-service-gateway:v1.0.0   
 docker-compose -f gw-docker-compose.yml up -d
 
 
@@ -18,7 +18,7 @@ sleep 20
 
 echo "mars-service-alioss"
 docker-compose -f oss-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-service-alioss:v1.0.0 
+docker rmi ip:5000/mars-service-alioss:v1.0.0 
 docker-compose -f oss-docker-compose.yml up -d
 
 sleep 20
@@ -27,7 +27,7 @@ sleep 20
 
 echo "mars-server-service-admin-provider"
 docker-compose -f admin-provider-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-server-service-admin-provider:v1.0.0  
+docker rmi ip:5000/mars-server-service-admin-provider:v1.0.0  
 docker-compose -f admin-provider-docker-compose.yml up -d
 
 sleep 20
@@ -35,7 +35,7 @@ sleep 20
 
 echo "mars-service-sso"
 docker-compose -f sso-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-service-sso:v1.0.0      
+docker rmi ip:5000/mars-service-sso:v1.0.0      
 docker-compose -f sso-docker-compose.yml up -d
 
 
@@ -44,7 +44,7 @@ sleep 20
 
 echo "mars-server-service-pay-provider"
 docker-compose -f pay-docker-compose.yml down   
-docker rmi 118.31.122.157:5000/mars-server-service-pay-provider:v1.0.0 
+docker rmi ip:5000/mars-server-service-pay-provider:v1.0.0 
 docker-compose -f pay-docker-compose.yml up -d
 
 
@@ -53,7 +53,7 @@ sleep 20
 echo "mars-server-service-order-provider"
 
 docker-compose -f order-provider-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-server-service-order-provider:v1.0.0      
+docker rmi ip:5000/mars-server-service-order-provider:v1.0.0      
 docker-compose -f order-provider-docker-compose.yml up -d
 
 sleep 20
@@ -61,7 +61,7 @@ sleep 20
 
 echo "mars-server-service-shop-provider"
 docker-compose -f shop-provider-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-server-service-shop-provider:v1.0.0   
+docker rmi ip:5000/mars-server-service-shop-provider:v1.0.0   
 docker-compose -f shop-provider-docker-compose.yml up -d
 
 
@@ -70,7 +70,7 @@ sleep 20
 
 echo "mars-server-service-user-provider"
 docker-compose -f user-provider-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-server-service-user-provider:v1.0.0  
+docker rmi ip:5000/mars-server-service-user-provider:v1.0.0  
 docker-compose -f user-provider-docker-compose.yml up -d
 
 
@@ -79,7 +79,7 @@ sleep 20
 
 echo "mars-server-service-admin-consumer"
 docker-compose -f admin-consumer-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-server-service-admin-consumer:v1.0.0  
+docker rmi ip:5000/mars-server-service-admin-consumer:v1.0.0  
 docker-compose -f admin-consumer-docker-compose.yml up -d
 
 
@@ -88,7 +88,7 @@ sleep 20
 
 echo "mars-server-service-order-consumer"
 docker-compose -f order-consumer-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-server-service-order-consumer:v1.0.0   
+docker rmi ip:5000/mars-server-service-order-consumer:v1.0.0   
 docker-compose -f order-consumer-docker-compose.yml up -d
 
 
@@ -97,7 +97,7 @@ sleep 20
 
 echo "mars-server-service-order-consumer"
 docker-compose -f shop-consumer-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-server-service-shop-consumer:v1.0.0   
+docker rmi ip:5000/mars-server-service-shop-consumer:v1.0.0   
 docker-compose -f shop-consumer-docker-compose.yml up -d
 
 
@@ -106,7 +106,7 @@ sleep 20
 
 echo "mars-server-service-order-consumer"
 docker-compose -f user-consumer-docker-compose.yml down
-docker rmi 118.31.122.157:5000/mars-server-service-user-consumer:v1.0.0  
+docker rmi ip:5000/mars-server-service-user-consumer:v1.0.0  
 docker-compose -f user-consumer-docker-compose.yml up -d
 
 
@@ -116,7 +116,7 @@ echo "部署完成"
 
 echo "mars-server-service-pay-consumer"
 docker-compose -f pay-consumer-docker-compose.yml down   
-docker rmi 118.31.122.157:5000/mars-server-service-pay-consumer:v1.0.0 
+docker rmi ip:5000/mars-server-service-pay-consumer:v1.0.0 
 docker-compose -f pay-consumer-docker-compose.yml up -d
 
 
